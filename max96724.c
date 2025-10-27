29ad40dc1dabd0c663d81a8b7748a95873311a06
jason.lai <jason.lai@analog.com>
2025-10-27 15:21:16 +0800

    feat: we're building ops functions, and check out almost part(need to check), next create max_des_remove, ops functions, lanes settings and register definitions

--- max96724.c
+++ max96724.c
@@ -1,4 +1,6 @@
 //need to check
+//need to note
+
 
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
@@ -163,12 +165,11 @@
 
 #define des_to_priv(_des)  container_of(_des, max96724_priv, des)
 
-//need to check
-struct regmap_config max96724_i2c_regmap = 
+static const struct regmap_config max96724_i2c_regmap = 
 {
     .reg_bits = 16,
     .val_bits = 8,
-    .max_register = 0x1f00,
+    .max_register = 0x1271, //default: 0x1f00, refer to datasheet 0x1271 is the latest register
 };
 
 
@@ -187,7 +188,7 @@ struct max96724_priv
     struct max_des des;
     const struct max96724_chip_info *info;
     struct device *dev;
-    struct i2c_client *clident;
+    struct i2c_client *client;
     struct regmap *regmap;
     struct gpio_desc *gpio_enable;
     struct gpio_desc *gpiod_pwdn;
@@ -209,6 +210,70 @@ static const struct max_phys_config max96724_phys_configs[] =
 
 };
 
+//need to check max_des_ops functions, we used des as an input parameter that is because functions will be called in max_des.c
+static int max96724_reg_read(struct max_des *des, unsigned int reg, unsigned int *val)
+{   
+    struct max96724_priv *priv = des_to_priv(des);
+    return regmap_read(priv->regmap, reg, val);
+}
+
+static int max96724_reg_write(struct max_des *des, unsigned int reg, unsigned int val)
+{
+    struct max96724_priv *priv = des_to_priv(des);
+    return regmap_write(priv->regmap, reg, val);
+}
+
+static int max96724_log_pipe_status(struct max_des* des, struct max_des_pipe *pipe, const char *name)
+{
+    struct max96724_priv *priv = des_to_priv(des);
+    unsigned int index = pipe->index;
+    unsigned int val, mask;
+    int ret;
+
+	ret = regmap_read(priv->regmap, MAX96724_VPRBS(index), &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \tvideo_lock: %u\n", name,
+		!!(val & MAX96724_VPRBS_VIDEO_LOCK));
+
+	mask = MAX96724_DET(index);
+
+	ret = regmap_read(priv->regmap, MAX96724_DE_DET, &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \tde_det: %u\n", name, !!(val & mask));
+
+	ret = regmap_read(priv->regmap, MAX96724_HS_DET, &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \ths_det: %u\n", name, !!(val & mask));
+
+	ret = regmap_read(priv->regmap, MAX96724_VS_DET, &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \tvs_det: %u\n", name, !!(val & mask));
+
+	ret = regmap_read(priv->regmap, MAX96724_HS_POL, &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \ths_pol: %u\n", name, !!(val & mask));
+
+	ret = regmap_read(priv->regmap, MAX96724_VS_POL, &val);
+	if (ret)
+		return ret;
+
+	pr_info("%s: \tvs_pol: %u\n", name, !!(val & mask));
+
+	return 0;
+}
+
+
+
 int max96724_set_pipe_tunnel_enable(struct max_des *des, struct max_des_pipe *pipe, bool enable)
 {
     struct max96724_priv* priv = des_to_priv(des);
@@ -228,20 +293,30 @@ static const struct max_des_ops max96724 =
 {
     .num_phys = 4,
     .num_links = 4,
-    .num_remaps_per_pipe = 16,   //need to check
+    .num_remaps_per_pipe = 16, 
     .phys_configs = 
     {
         .num_configs = ARRAY_SIZE(max96724_phys_configs),
         .configs = max96724_phys_configs,
     },
     .use_atr = true,
-    // .reg_read = max96724_reg_read,
-    // .reg_write = max96724_reg_write,
-    // .log_pipe_status = max96724_log_pipe_status,
-    // .log_phy_status = max96724_log_phy_status,
-    //need to add whole functions
-    .init = max96724_init,
-
+	.reg_read = max96724_reg_read,
+	.reg_write = max96724_reg_write,
+	.log_pipe_status = max96724_log_pipe_status,
+	.log_phy_status = max96724_log_phy_status,
+	.set_enable = max96724_set_enable,
+	.init = max96724_init,
+	.init_phy = max96724_init_phy,
+	.set_phy_mode = max96724_set_phy_mode,
+	.set_phy_active = max96724_set_phy_active,
+	.set_pipe_phy = max96724_set_pipe_phy,
+	.set_pipe_stream_id = max96724_set_pipe_stream_id,
+	.set_pipe_enable = max96724_set_pipe_enable,
+	.set_pipe_remap = max96724_set_pipe_remap,
+	.set_pipe_remaps_enable = max96724_set_pipe_remaps_enable,
+	.set_pipe_mode = max96724_set_pipe_mode,
+	.select_links = max96724_select_links,
+	.set_link_version = max96724_set_link_version,
 
 };
 
@@ -269,11 +344,49 @@ static const struct max96724_chip_info max96724f_info =
 
 
 
-//we should build default
 
+//wait for SoC that can read right device address.
+static int max96724_wait_for_device(struct max96724_priv *priv)
+{   
+    int ret;
 
+    for(i = 0; i < 10; i++)
+    {
+        unsigned int val = 0;
+        /*MAX96724_REG0 : 7-1: device address 0: block*/
+        ret = regmap_read(priv->regmap, MAX96724_REG0, &val);
+        if(!ret && (val >> 1) == priv->client->addr)
+            return 0;
+
+        msleep(100);
+
+        dev_err(priv->dev, "Retry %u waiting for deserializer: ret->%d, val->%d\n",i , ret, val);
+    }
+
+    return ret;
+}
+
+//reset chip operation
 static int max96724_reset(struct max96724_priv *priv)
 {
+
+    int ret;
+
+    ret = max96724_wait_for_device(priv);
+    if(ret)
+        return ret;
+
+    ret = regmap_update_bits(priv->regmap, MAX96724_PWR1, MAX96724_PWR1_RESET_ALL,
+                             FIELD_PREP(MAX96724_PWR1_RESET_ALL, 1));
+    if(ret)
+        return ret;
+
+    fsleep(10000);
+
+    ret = max96724_wait_for_device(priv);
+    if(ret)
+        return ret;
+
     return 0;
 }
 
@@ -294,8 +407,7 @@ static int max96724_probe(struct i2c_client *client)
     ops = devm_kzalloc(priv->dev, sizeof(*ops), GFP_KERNEL);
     if(!ops)
         return -ENOMEM;
-
-    //need to check (get data from dt and compare with our info table)
+    /*get device's info from corresponding "of match table"*/
     priv->info = device_get_match_data(dev);
     if(!priv->info)
     {
@@ -303,26 +415,25 @@ static int max96724_probe(struct i2c_client *client)
         return -ENODEV;
     }
 
+    /*connect each other*/
     priv->dev = dev;
     priv->client = client;
     i2c_set_clientdata(client, priv);
 
-    //need to check init regmap method and is_err/ptr_err functions
+    //connect i2c to regmap with max96724 format and send it to priv structure
     priv->regmap = devm_regmap_init_i2c(client, &max96724_i2c_regmap);
     if(IS_ERR(priv->regmap))
         return PTR_ERR(priv->regmap);
 
-    //need to check this function and dev/&client->dev
+    //need to note this pin
     priv->gpiod_enable = devm_gpiod_get_optional(dev, "enable", GPIOD_OUT_HIGH);
     if(IS_ERR(priv->gpiod_enable))
         return PTR_ERR(priv->gpiod_enable);
 
-    //need to check sleep function, and need to check the spec
     if(priv->gpio_enable)
         usleep_range(4000, 5000);
 
 
-    //need to check get_optional function / cansleep; and check the spec
     priv->gpiod_pwdn = devm_gpiod_get_optional(dev, "powerdown", GPIOD_OUT_HIGH);
     if(IS_ERR(priv->gpiod_pwdn))
         return PTR_ERR(priv->gpiod_pwdn);
@@ -335,14 +446,13 @@ static int max96724_probe(struct i2c_client *client)
         usleep_range(4000, 5000);
     }
 
-    //need to check whole assignments
+
     *ops = max96724_ops;
     ops->version = priv->info->versions;
     ops->num_pipes = priv->info->num_pipes;
     ops->set_pipe_tunnel_enable = priv->info->set_pipe_tunnel_enable;
     priv->des.ops = ops;
 
-    //need to check
     ret = max96724_reset(priv);
     if(ret)
         return ret;
@@ -352,14 +462,13 @@ static int max96724_probe(struct i2c_client *client)
 }
 
 /*oppsite of probe is remove*/
-static max96724_remove(struct i2c_client *client)
+static void max96724_remove(struct i2c_client *client)
 {
     struct max96724_priv *priv = i2c_get_clientdata(client);
 
-    //need to check
+    /*need to note, we should create this function*/
     max_des_remove(&priv->des);
 
-    //need to check
     gpiod_set_value_cansleep(priv->gpiod_pwdn, 1);
     gpiod_set_value_cansleep(priv->gpiod_enable, 0);
 }
@@ -373,7 +482,7 @@ static const struct of_device_id max96724_of_table[] =
     //{ .compatible = "maxim,max96724r", .data=&max96724f_info}
 };
 
-//need to check
+//this is use for denoting ID table to kernel module
 MODULE_DEVICE_TABLE(of, max96724_of_table);
 
 static struct i2c_driver max96724_i2c_driver = 
@@ -392,5 +501,4 @@ module_i2c_driver(max96724_i2c_driver);
 MODULE_DESCRIPTION("Analog devices MAX96724 Quad GMSL2 Deserializer Driver");
 MODULE_AUTHOR("Cosmin Tanislav <cosmin.tanislav@analog.com>");
 MODULE_AUTHOR("Jason Lai <jason.lai@analog.com>");
-//need to check
 MODULE_LICENSE("GPL");
